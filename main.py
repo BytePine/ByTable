2edc1a91a31f92b079cb213f8445a49cb8ddde15
byteyang <bytepine@gmail.com>
2022-11-10 20:50:14 +0800

    添加主函数

---
+++ main.py
@@ -0,0 +1,7 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(levelname)s: %(message)s')
+
+if __name__ == '__main__':
+    logging.debug('Hello ByTable')
