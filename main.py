f438a37aa6efa5cfd1ea9c2a96d11933067bef73
byteyang <bytepine@gmail.com>
2022-11-28 00:41:50 +0800

    [加载]拆分加载参数

--- main.py
+++ main.py
@@ -7,5 +7,7 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(levelname)s: %(message)s')
 
 if __name__ == '__main__':
+    # 加载参数
+    load_param = LoadParam()
     # 加载资源
-    Load(LoadParam())
+    Load(load_param)
